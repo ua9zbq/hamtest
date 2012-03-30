45d4981bbd41bbddd83467695dc9c4539af6366d
ua9zbq <ua9zbq@ngs.ru>
2012-03-30 13:54:41 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hamtest::Application.config.secret_token = '6e16c5a736ff6de42e453dae79a8f15817cc901f7d427af18b4fd40abeb8c6929fc2b25c351d8c6c9d9f3724e3a4cee630fa1fd16d8d68febccad9c2e3a0a81b'
